aa318cdede8eba8c0a2fd2ec7649886a5377fc7d
karencci <2024434@student.cct.ie>
2025-05-07 12:35:44 +0100

    First commit

---
+++ CA_2TechCompany.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca_2techcompany;
+
+/**
+ *
+ * @author Administrator
+ */
+public class CA_2TechCompany {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Employee.java
@@ -0,0 +1,14 @@
+package ca_2techcompany;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class Employee {
+    
+}
